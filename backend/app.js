3b76a369cf8eae27d18f3a08b7db0b1179e41745
Alexandr Zuev <zuevalex@yandex.ru>
2023-07-06 18:47:56 +0500

    README

--- backend/app.js
+++ backend/app.js
@@ -40,6 +40,12 @@ app.use(cookieParser());
 
 app.use(requestLogger); // подключаем логгер запросов
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(router);
 
 app.use(errorLogger); // подключаем логгер ошибок
