b72d30f8df7cd86b9eeb7aba6de4ab6b6242a8ef
Alexandr Zuev <zuevalex@yandex.ru>
2023-07-08 18:18:57 +0500

    опять cors2

--- backend/middlewares/optionsCors.js
+++ backend/middlewares/optionsCors.js
@@ -54,37 +54,37 @@
 //   optionsSuccessStatus: 200,
 // };
 
-const allowedCors = [
-  'https://zuevmesto.students.nomoreparties.sbs',
-  'http://zuevmesto.students.nomoreparties.sbs',
-  'localhost:3000',
-  'http://localhost:3000',
-];
+// const allowedCors = [
+//   'https://zuevmesto.students.nomoreparties.sbs',
+//   'http://zuevmesto.students.nomoreparties.sbs',
+//   'localhost:3000',
+//   'http://localhost:3000',
+// ];
 
-app.use((req, res, next) => {
-  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-  // проверяем, что источник запроса есть среди разрешённых
-  if (allowedCors.includes(origin)) {
-    // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
-  }
+// app.use((req, res, next) => {
+//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+//   // проверяем, что источник запроса есть среди разрешённых
+//   if (allowedCors.includes(origin)) {
+//     // устанавливаем заголовок, который разрешает браузеру запросы с этого источника
+//     res.header('Access-Control-Allow-Origin', origin);
+//     res.header('Access-Control-Allow-Credentials', true);
+//   }
 
-  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
-  // сохраняем список заголовков исходного запроса
-  const requestHeaders = req.headers['access-control-request-headers'];
+//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+//    Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+//   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+//   // сохраняем список заголовков исходного запроса
+//   const requestHeaders = req.headers['access-control-request-headers'];
 
-  // Если это предварительный запрос, добавляем нужные заголовки
-  if (method === 'OPTIONS') {
-    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-    // разрешаем кросс-доменные запросы с этими заголовками
-    res.header('Access-Control-Allow-Headers', requestHeaders);
-    // завершаем обработку запроса и возвращаем результат клиенту
-    return res.end();
-  }
+//   // Если это предварительный запрос, добавляем нужные заголовки
+//   if (method === 'OPTIONS') {
+//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+//     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+//     // разрешаем кросс-доменные запросы с этими заголовками
+//     res.header('Access-Control-Allow-Headers', requestHeaders);
+//     // завершаем обработку запроса и возвращаем результат клиенту
+//     return res.end();
+//   }
 
-  next();
-});
+//   next();
+// });
